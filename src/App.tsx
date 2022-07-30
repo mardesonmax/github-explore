5c9ddf9ff373025fb9d67e39c9bf0354c6b19d33
Mardeson Pereira <maxpb777@gmail.com>
2022-07-30 08:50:54 -0300

    feat: create global styles

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,12 @@ import { ThemeProvider } from 'styled-components';
 import { Home } from './pages/home';
 import { theme } from './styles/theme';
 
+import GlobalStyles from './styles/global';
+
 export const App: React.FC = () => (
   <ThemeProvider theme={theme}>
     <Home />
+
+    <GlobalStyles />
   </ThemeProvider>
 );
