414ca7eada4142fe8936d0a84d4ec101cf9a5c5c
Mardeson Pereira <maxpb777@gmail.com>
2022-07-30 08:34:49 -0300

    feat: create theme provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,10 @@
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import { Home } from './pages/home';
+import { theme } from './styles/theme';
 
-export const App: React.FC = () => <div>Hello</div>;
+export const App: React.FC = () => (
+  <ThemeProvider theme={theme}>
+    <Home />
+  </ThemeProvider>
+);
---
+++ src/styles/theme/index.ts
@@ -0,0 +1,10 @@
+export const theme = {
+  colors: {
+    white: '#FFFFFF',
+    'gray-100': '#F2F2FA',
+    'gray-700': '#3D3D4D',
+    'gray-800': '#3A3A3A',
+    'gray-400': '#A8A8B3',
+    'green-200': '#04D361',
+  },
+};
