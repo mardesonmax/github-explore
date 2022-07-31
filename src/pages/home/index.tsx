8f1cae4873a8647b86e3155b72fedfd4f398def4
Mardeson Pereira <maxpb777@gmail.com>
2022-07-31 13:25:47 -0300

    feat: integration with respository list

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -3,41 +3,48 @@ import { Header } from '~/components/header';
 import { Search } from '~/components/search';
 import { FaChevronRight } from 'react-icons/fa';
 
+import { useRepository } from '~/hooks/repository';
+import { createPath, createSearchParams } from 'react-router-dom';
 import { Container, Content, ContentHeader, Cards, Card } from './styles';
 
-export const Home: React.FC = () => (
-  <Container>
-    <Header />
+export const Home: React.FC = () => {
+  const { repositories } = useRepository();
+  return (
+    <Container>
+      <Header />
 
-    <Content>
-      <ContentHeader>
-        <h1>
-          Explore repositórios <br />
-          no Github.
-        </h1>
+      <Content>
+        <ContentHeader>
+          <h1>
+            Explore repositórios <br />
+            no Github.
+          </h1>
 
-        <Search />
-      </ContentHeader>
+          <Search />
+        </ContentHeader>
 
-      <Cards>
-        <Card to="/repository">
-          <div className="avatar">
-            <img
-              src="https://avatars.githubusercontent.com/u/45460698?v=4"
-              alt=""
-            />
-          </div>
+        <Cards>
+          {repositories.map(repository => (
+            <Card
+              to={`repository?repo=${repository.fullName.trim()}`}
+              key={repository.id}
+            >
+              <div className="avatar">
+                <img src={repository.avatarUrl} alt="Avatar" />
+              </div>
 
-          <div className="details">
-            <h2>mardesonmax/repo</h2>
-            <p>Descrição do repo</p>
-          </div>
+              <div className="details">
+                <h2>{repository.fullName}</h2>
+                {repository.description && <p>{repository.description}</p>}
+              </div>
 
-          <div className="icon">
-            <FaChevronRight />
-          </div>
-        </Card>
-      </Cards>
-    </Content>
-  </Container>
-);
+              <div className="icon">
+                <FaChevronRight />
+              </div>
+            </Card>
+          ))}
+        </Cards>
+      </Content>
+    </Container>
+  );
+};
