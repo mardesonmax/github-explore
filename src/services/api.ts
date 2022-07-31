3ae316eb557c2ec4c8ef14bfad30dc9affbd0930
Mardeson Pereira <maxpb777@gmail.com>
2022-07-31 17:24:42 -0300

    feat: add github token

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,10 @@
 import axios from 'axios';
 
+const accessToken = process.env.REACT_APP_GITHUB_ACCESS_TOKEN;
+
 export const api = axios.create({
+  headers: {
+    Authorization: `token ${accessToken}`,
+  },
   baseURL: 'https://api.github.com',
 });
